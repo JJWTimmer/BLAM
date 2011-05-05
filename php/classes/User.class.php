8ecb010ae0af276ce62f0fd935a7de7019dc21eb
Jasper Timmer <jjwtimmer@gmail.com>
2011-05-05 23:06:49 +0200

    removed logmsg

--- php/classes/User.class.php
+++ php/classes/User.class.php
@@ -65,7 +65,7 @@ class User extends RVDLogBase {
             unset($this->password);
             
             if ( $dbresult->num_rows == 1 ) {
-				logmsg($user);
+
                 $user = $dbresult->fetch_array();
                 $this->id       = $user['id'];
                 $this->username = $user['username'];
